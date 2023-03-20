218ce10be4c7ccb27e91458e8839fdb03361708c
OneClock1 <toly1539@gmail.com>
2023-03-20 23:25:13 +0200

    Create Server

---
+++ backend/Server/Server/Controllers/ProductsController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly ProductsAPIDbContext dbContext;
+
+        public ProductsController(ProductsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return Ok(await dbContext.Products.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetProduct([FromRoute] Guid id)
+        {
+            var product = await dbContext.Products.FindAsync(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+ 
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(AddProductRequest addProductRequest) 
+        {
+            var product = new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = addProductRequest.Name,
+                Description = addProductRequest.Description,
+                Price = addProductRequest.Price
+            };
+            await dbContext.Products.AddAsync(product);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(product);
+
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateProduct([FromRoute] Guid id, UpdateProductRequest updateProductRequest)
+        {
+            var product = await dbContext.Products.FindAsync(id);
+
+            if (product == null) 
+            {
+                return NotFound();
+            }
+
+            product.Name = updateProductRequest.Name;
+            product.Description = updateProductRequest.Description;
+            product.Price = updateProductRequest.Price;
+
+            await dbContext.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteProduct([FromRoute] Guid id)
+        {
+            var product = await dbContext.Products.FindAsync(id);
+            if (product != null)
+            {
+                dbContext.Remove(product);
+                await dbContext.SaveChangesAsync();
+                return Ok(product);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ backend/Server/Server/Data/ProductsAPIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Server.Data
+{
+    public class ProductsAPIDbContext : DbContext
+    {
+        public ProductsAPIDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+
+    
+}
