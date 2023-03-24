19909fdca1904180a165f05c24e8dd2a46fff786
OneClock1 <toly1539@gmail.com>
2023-03-24 23:40:15 +0200

    Update UpdateProductRequest.cs

--- backend/Server/Server/Models/UpdateProductRequest.cs
+++ backend/Server/Server/Models/UpdateProductRequest.cs
@@ -2,7 +2,7 @@
 {
     public class UpdateProductRequest
     {
-        public string Name { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
     }
