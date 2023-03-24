d9599111abd3fcbe2d3c1fdca8dc9ff8d484c519
OneClock1 <toly1539@gmail.com>
2023-03-24 23:47:22 +0200

    Update App.js

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -8,22 +8,22 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Shop from './components/Shop/Shop';
 import AddProduct from './components/AddProduct/AddProduct';
-import EditProduct from './components/EditProduct/EditProduct';
+import EditProduct from './components/EditProduct/EditProduct'
 
 function App() {
-  // const [products, setProducts] = useState([
-  //   {id: 1, title: 'Product 1', price: 899},
-  //   {id: 2, title: 'Product 2', price: 982},
-  //   {id: 3, title: 'Product 3', price: 322},
-  //   {id: 4, title: 'Product 4', price: 763},
-  //   {id: 5, title: 'Product 5', price: 389}
-  // ]);
+  const [products, setProducts] = useState([
+    {id: 1, title: 'Product 1', price: 899},
+    {id: 2, title: 'Product 2', price: 982},
+    {id: 3, title: 'Product 3', price: 322},
+    {id: 4, title: 'Product 4', price: 763},
+    {id: 5, title: 'Product 5', price: 389}
+  ]);
   
 
-  // const deleteProduct = (productId) => {
-  //   const newProduct = products.filter(product => product.id !== productId);
-  //   setProducts(newProduct);
-  // }
+  const deleteProduct = (productId) => {
+    const newProduct = products.filter(product => product.id !== productId);
+    setProducts(newProduct);
+  }
 
   return (
     
@@ -36,7 +36,8 @@ function App() {
         <Route path='/about' element={<About/>}></Route>   
         <Route path='/shop' element={<Shop/>}></Route>  
         <Route path='/add' element={<AddProduct/>}></Route>  
-        <Route path='/edit/:id' element={<EditProduct/>}></Route>      
+        <Route path='/edit/:id' element={<EditProduct/>}></Route> 
+        <Route path='/products' element={<Productlist/>}></Route>     
        </Routes>
         {/* <Productlist products={products} deleteProduct={ deleteProduct } /> */}
          
